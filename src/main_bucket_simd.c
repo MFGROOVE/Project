29a02abf164880c816ce4b285ef68a6206db7131
Blkswn <blkswn@archlinux.localdomain>
2025-09-14 23:25:10 -0300

    FEAT:Agora as cada codigo recebe como parametro o numero de threads e o tamanho do bloco respectivamente

--- src/main_bucket_simd.c
+++ src/main_bucket_simd.c
@@ -4,14 +4,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-  srand(42); /* As matrizes são aleátorias,mas,
-     são controladas pela seed,
-     assim todos os algoritmos realizam a multiplicação com a mesma matriz */
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    printf("Faltam argumentos para o codigo. Os argumentos são:\n1) numero de "
+           "threads\n2) tamanho da matriz\n");
+    return 1;
+  }
 
-  int (*matrix_a)[N] = malloc(sizeof(int[N][N]));
-  int (*matrix_b)[N] = malloc(sizeof(int[N][N]));
-  int (*matrix_c)[N] = malloc(sizeof(int[N][N]));
+  srand(42);
+
+  int num_threads = atoi(argv[1]);
+  int S = atoi(argv[2]);
+  int (*matrix_a)[S] = malloc(sizeof(int[S][S]));
+  int (*matrix_b)[S] = malloc(sizeof(int[S][S]));
+  int (*matrix_c)[S] = malloc(sizeof(int[S][S]));
 
   fill_random_matrix(matrix_a);
   fill_random_matrix(matrix_b);
@@ -19,24 +25,26 @@ int main() {
 
   double start_time = omp_get_wtime();
 
+  omp_set_num_threads(num_threads);
+
 #pragma omp parallel
   {
     int n = omp_get_num_threads();
     int id = omp_get_thread_num();
-    int bucket = N / n;
+    int bucket = S / n;
     int start = id * bucket;
     int end;
 
     if (id == n - 1) {
-      end = N;
+      end = S;
     } else {
       end = start + bucket;
     }
 
     for (int i = start; i < end; i++) {
-      for (int j = 0; j < N; j++) {
+      for (int j = 0; j < S; j++) {
 #pragma omp simd
-        for (int k = 0; k < N; k++) {
+        for (int k = 0; k < S; k++) {
           matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
         }
       }
@@ -46,15 +54,19 @@ int main() {
   double end_time = omp_get_wtime();
   printf("Tempo de execucao (metodo buckets + SIMD): %f segundos\n",
          end_time - start_time);
+  double time = end_time - start_time;
+
   free(matrix_a);
   free(matrix_b);
-  FILE *file = fopen("Metodo_bucket_SIMD ", "w");
-  for (size_t x = 0; x < N; x++) {
-    for (size_t y = 0; y < N; y++) {
+
+  FILE *file = fopen("Metodo_bucket_SIMD", "w");
+  for (size_t x = 0; x < S; x++) {
+    for (size_t y = 0; y < S; y++) {
       fwrite(&matrix_c[x][y], sizeof(int), 1, file);
     }
   }
-
+  fclose(file);
+  printf("%f", time);
   free(matrix_c);
   return 0;
 }
