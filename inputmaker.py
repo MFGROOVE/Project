db67754a27d7caf9ab83c44d896dcb6128803d60
Blkswn <blkswn@archlinux.localdomain>
2025-10-06 23:57:35 -0300

    refactor:Removed the processor parameter of the csv

--- inputmaker.py
+++ inputmaker.py
@@ -1,16 +1,15 @@
 import itertools
 import csv
 
-processador = ["AMD Ryzen 7 5700X"]
 estrategias = ["sequencial", "id", "buckets", "id-simd", "buckets-simd"]
 dimensoes = [1000, 100, 10]
 threads = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 input_archive = "input.csv"
-cabecalho = ["processador", "estrategias", "threads", "dimensoes(N X N)"]
+cabecalho = ["estrategias", "threads", "dimensoes(N X N)"]
 
-todas_combinacoes = itertools.product(processador, estrategias, threads, dimensoes)
+todas_combinacoes = itertools.product(estrategias, threads, dimensoes)
 i = 0
 
 try:
@@ -19,11 +18,10 @@ try:
         input.writerow(cabecalho)
 
         for combinacao in todas_combinacoes:
-            linha = [combinacao[0], combinacao[1], combinacao[2], combinacao[3]]
+            linha = [combinacao[0], combinacao[1], combinacao[2]]
             input.writerow(linha)
             i += 1
 
-    print(f"Arquivo escrito em: {input_archive}, com:", i, "combinações")
 
 except IOError:
     print(f"Erro: Não foi possível escrever no arquivo {input_archive}")
