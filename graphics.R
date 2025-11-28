f13ea5989d22c7879de9979fc42443af025c8d71
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2025-11-27 23:59:53 -0300

    one plot to rule them all

--- graphics.R
+++ graphics.R
@@ -1,44 +1,12 @@
 library(tidyverse)
 
-csv <- read.csv("resultados.csv")
-plot_1000 <- csv |>
-        filter(dimensoes.N.X.N. == 1000)
-ggplot(data = plot_1000, mapping = aes(x = threads, y = tempo, color = estrategias)) +
+read.csv("resultados.csv") |>
+ggplot(mapping = aes(x = threads, y = tempo, color = estrategias, group=dimensoes.N.X.N.)) +
         geom_line() +
         geom_point() +
-        facet_wrap(~estrategias, nrow = 1) +
+        facet_grid(dimensoes.N.X.N.~estrategias, scales="free_y") +
         labs(
-                title = "Desempenho por Número de Threads (Matriz 1000x1000)",
-                subtitle = "Comparação entre diferentes estratégias de paralelização",
-                x = "Número de Threads",
-                y = "Tempo de Execução (segundos)",
-                color = "Estratégia"
-        ) +
-    ylim(0, NA)
-
-plot_100 <- csv |>
-        filter(dimensoes.N.X.N. == 100)
-ggplot(data = plot_100, mapping = aes(x = threads, y = tempo, color = estrategias)) +
-        geom_line() +
-        geom_point() +
-        facet_wrap(~estrategias, nrow = 1) +
-        labs(
-                title = "Desempenho por Número de Threads (Matriz 100x100)",
-                subtitle = "Comparação entre diferentes estratégias de paralelização",
-                x = "Número de Threads",
-                y = "Tempo de Execução (segundos)",
-                color = "Estratégia"
-        ) +
-    ylim(0, NA)
-
-plot_10 <- csv |>
-        filter(dimensoes.N.X.N. == 10)
-ggplot(data = plot_10, mapping = aes(x = threads, y = tempo, color = estrategias)) +
-        geom_line() +
-        geom_point() +
-        facet_wrap(~estrategias, nrow = 1) +
-        labs(
-                title = "Desempenho por Número de Threads (Matriz 10x10)",
+                title = "Desempenho por Número de Threads",
                 subtitle = "Comparação entre diferentes estratégias de paralelização",
                 x = "Número de Threads",
                 y = "Tempo de Execução (segundos)",
